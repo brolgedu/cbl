65191fc96892562efeb39500ccd70aabc5a8eea4
broel <joelthomas.e@gmail.com>
2021-12-23 08:24:15 -0800

    Removed __OBJC__ directive.

--- src/cbl.h
+++ src/cbl.h
@@ -3,19 +3,7 @@
 //////////////////////////////////////////
 //      Used by client application      //
 //////////////////////////////////////////
-
-#ifdef __cplusplus
-
 #include "Core/CBLFileSystem.h"
 #include "Core/CBLClipboard.h"
 #include "Core/CBLEvents.h"
 #include "Core/CBLTime.h"
-
-#elif defined(__OBJC__)
-
-#import "Core/CBLFileSystem.h"
-#import "Core/CBLClipboard.h"
-#import "Core/CBLEvents.h"
-#import "Core/CBLTime.h"
-
-#endif
