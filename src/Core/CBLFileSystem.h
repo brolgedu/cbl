43e4c17ae01fb8504a5e94125c8d622367307208
broel <joelthomas.e@gmail.com>
2021-12-23 07:53:13 -0800

    Abstracted filesystem class to handle all things filesystem -- including creating/reading/and writing to files.

--- src/Core/CBLFileSystem.h
+++ src/Core/CBLFileSystem.h
@@ -1,36 +1,79 @@
 #pragma once
 
+#include "CBLTime.h"
+
 #import <Foundation/NSPathUtilities.h>
 #import <Foundation/NSFileManager.h>
 #import <Foundation/NSFileHandle.h>
+
+#import <memory>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef unsigned long long ULONG;
+typedef unsigned int UINT;
+
 @interface CBLFileSystem : NSObject {
+
 @private
     void *mAutoreleasePool;
-    const char* mFilePath;
+
+    CBLTime mTime;
+
+    NSFileManager *mFileManager;
+    NSFileHandle *mFileHandle;
+
+    NSMutableString *mFilePath;
+    NSMutableString *mFileName;
+
+    NSString *mCBLDirectory;
+    NSString *mAppSupportDirectory;
+    NSString *mCBLHistoryDirectory;
+
+    ULONG mFileContentsLength;
+    ULONG mLengthOfLastWrite;
+
+    UINT mNumberOfEntries;
 }
 
 - (id)init;
 - (void)dealloc;
 
-- (const char *)GetPathForDirectory:(NSSearchPathDirectory)directory :(NSSearchPathDomainMask)domainMask;
-- (const char*)GetFilePath;
-- (const char *)CreateFileAtPath:(const char *)filePathDirectory :(const char *)fileName :(const char *)fileType :(const char *)data;
+- (const char *)GetFilePath;
+- (const char *)GetDirectoryPath:(const char *)filePath;
+- (const char *)NSGetPathForDirectory:(NSSearchPathDirectory)directory :(NSSearchPathDomainMask)domainMask;
 
-- (BOOL)FileExistsAtPath:(const char*)filePath;
-- (BOOL)CreateDirectoryAtPath:(const char *)directoryPath;
-- (BOOL)OverwriteFileAtPath:(const char *)filePath :(const char *)data;
-- (BOOL)AppendFileAtPath:(const char *)filePath :(const char *)data;
-- (BOOL)RemoveTextFromFileAtPath:(const char *)filePath :(const char *)data;
+- (void)TailFile:(const char *)filePath;
+- (const char *)ReadFileStream:(const char *)filePath;
+- (const char *)GetContentsOfFileAtPath:(const char *)filePath;
 
-- (NSData *)GetContentsOfFileAtPath:(const char *)filePath;
+- (bool)CreateDirectoryAtPath:(const char *)directoryPath;
+- (bool)CreateFileAtPath:(const char *)filePathDirectory :(const char *)fileName :(const char *)contents;
+- (bool)CreateFileAtPath:(const char *)filePath :(const char *)contents;
+- (bool)CreateFileAtPath:(const char *)filePath;
+- (bool)CreateFileAtDefaultPath;
 
-- (const char*)NSStringToCString:(NSString*)nsString;
-- (NSString*)CStringToNSString:(const char*)cString;
+- (bool)OpenFileAtPath:(const char *)filePath;
+- (bool)FileExistsAtPath:(const char *)filePath;
+- (bool)RemoveTextFromFileAtPath:(const char *)filePath :(const char *)contents;
+- (bool)AppendFileAtPathWithContent:(const char *)filePath :(const char *)contents;
+- (bool)OverwriteFileAtPathWithContent:(const char *)filePath :(const char *)contents;
+
+- (ULONG)GetFileContentsLength;
+
+
+//////////////////////////////////////////////////////////
+//                  Helper Functions                    //
+//////////////////////////////////////////////////////////
+
+- (NSString *)CStringToNSString:(const char *)cString;
+- (const char *)NSStringToCString:(NSString *)nsString;
+
+- (const char*)WrapTimeStamp:(const char *)contents;
+- (const char *)RemoveLeadingWhiteSpace:(const char *)contents;
+
 
 @end
 
--- src/Core/CBLFileSystem.mm
+++ src/Core/CBLFileSystem.mm
@@ -1,13 +1,38 @@
+#import "cblpch.h"
 #import "CBLFileSystem.h"
 
-#import "Core.h"
+#import "Core/Core.h"
 
 #import <Foundation/Foundation.h>
 
 @implementation CBLFileSystem
+
 - (id)init {
     self = [super init];
+
     mAutoreleasePool = [[NSAutoreleasePool alloc] init];
+    mFileManager = [NSFileManager defaultManager];
+    mFileHandle = [[NSFileHandle alloc] init];
+
+    mAppSupportDirectory = [NSString stringWithUTF8String:(
+            [self NSGetPathForDirectory:(NSApplicationSupportDirectory) :(NSUserDomainMask)])];
+
+    mCBLDirectory = [mAppSupportDirectory stringByAppendingPathComponent:@"/cbl"];
+    mCBLHistoryDirectory = [mCBLDirectory stringByAppendingPathComponent:@"/history"];
+
+    mFileName = [@"history.txt" mutableCopy];
+    mFilePath = [[mCBLHistoryDirectory stringByAppendingPathComponent:
+                                               [NSString stringWithFormat:@"/%@", mFileName]] mutableCopy];
+    CBLLog(@"[mFilePath]: %@", mFilePath);
+
+
+    if (![self FileExistsAtPath:[self NSStringToCString:mFilePath]]) {
+        if (![self CreateFileAtDefaultPath]) {
+            CBLLog(@"[CreateFileAtDefaultPath]: Error creating file!");
+            return nil;
+        }
+    }
+
     return self;
 }
 
@@ -16,7 +41,7 @@
     [super dealloc];
 }
 
-- (const char *)GetPathForDirectory:(NSSearchPathDirectory)directory :(NSSearchPathDomainMask)domainMask {
+- (const char *)NSGetPathForDirectory:(NSSearchPathDirectory)directory :(NSSearchPathDomainMask)domainMask {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(directory, domainMask, YES);
 
     if (paths.count == 0) {
@@ -31,132 +56,281 @@
 }
 
 - (const char *)GetFilePath {
-    return mFilePath;
+    return mFilePath.fileSystemRepresentation;
 }
 
-- (BOOL)FileExistsAtPath:(const char *)filePath {
-    return [[NSFileManager defaultManager] fileExistsAtPath:[NSString stringWithUTF8String:(filePath)]];
+- (bool)OpenFileAtPath:(const char *)filePath {
+    if (filePath != nil) {
+        mFilePath = [[self CStringToNSString:filePath] mutableCopy];
+        mFileHandle = [NSFileHandle fileHandleForUpdatingAtPath:mFilePath];
+        mFileContentsLength = [mFileHandle seekToEndOfFile];
+        [mFileHandle seekToFileOffset:0];
+        return true;
+    }
+
+    return false;
 }
 
-- (BOOL)CreateDirectoryAtPath:(const char *)directoryPath {
+- (ULONG)GetFileContentsLength {
+    return mFileContentsLength;
+}
+
+- (bool)FileExistsAtPath:(const char *)filePath {
+    return [mFileManager fileExistsAtPath:[self CStringToNSString:filePath]];
+}
+
+/* If directoryPath has a file extension, then remove the file extension
+ * and name from the path to get the directory */
+- (const char *)GetDirectoryPath:(const char *)filePath {
+    ULONG pathLength = std::strlen(filePath);
+
+    char *directoryPath = (char *) std::malloc((pathLength * sizeof(filePath)));
+    std::strcpy(directoryPath, filePath);
+
+    UINT length = pathLength - 1;
+    UINT i = length;
+    bool done = false;
+
+    while (i != 0 && !done) {
+        switch (directoryPath[i]) {
+            case '/': done = true;
+                break;
+            case '.':
+                while (i > 0 && !done) {
+                    --i;
+                    if (directoryPath[i] == '/') {
+                        directoryPath[i] = '\0';
+                        done = true;
+                    }
+                }
+                break;
+        }
+        --i;
+    }
+    return directoryPath;
+}
+
+- (bool)CreateDirectoryAtPath:(const char *)directoryPath {
     NSError *error = 0;
     if (directoryPath) {
-        if (!FileExistsAtPath:(directoryPath)) {
-            [[NSFileManager defaultManager] createDirectoryAtPath:[NSString stringWithUTF8String:directoryPath]
-                                      withIntermediateDirectories:YES attributes:nil error:&error];
+        if (![self FileExistsAtPath:directoryPath]) {
+            [mFileManager createDirectoryAtPath:[self CStringToNSString:directoryPath]
+                    withIntermediateDirectories:YES attributes:nil error:&error];
             if (error) {
-                CBLLog(@"[NSFileManager]: error creating directory: %@", error);
+                CBLLog(@"[CreateDirectoryAtPath]: error creating directory: %@", error);
             }
         } else {
-            CBLLog(@"[NSFileManager]: directory already exists!");
+            CBLLog(@"[CreateDirectoryAtPath]: directory already exists!");
         }
     }
 
     return !error ? true : false;
 }
 
-- (NSData *)GetContentsOfFileAtPath:(const char *)filePath {
-    NSData *data;
+- (const char *)GetContentsOfFileAtPath:(const char *)filePath {
+    NSData *contents;
 
-    if ([[NSFileManager defaultManager] fileExistsAtPath:[NSString stringWithUTF8String:(filePath)]]) {
-        data = [[NSData alloc] initWithContentsOfFile:[NSString stringWithUTF8String:(filePath)]];
+    if ([self FileExistsAtPath:filePath]) {
+        contents = [[NSData alloc] initWithContentsOfFile:[self CStringToNSString:filePath]];
     } else {
-        data = [[NSData alloc] init];
-        CBLLog(@"[NSFileManager]: File not found!");
+        contents = [[NSData alloc] init];
+        CBLLog(@"[GetContentsOfFileAtPath]: File not found!");
     }
 
-    return data;
+    return (const char *) contents;
 }
 
 - (const char *)NSStringToCString:(NSString *)nsString {
+    return [nsString UTF8String];
 }
 
 - (NSString *)CStringToNSString:(const char *)cString {
-    return nil;
+    return [NSString stringWithUTF8String:cString];
 }
 
-
 // Creates and writes contents to file
-- (const char *)CreateFileAtPath:(const char *)directoryPath :(const char *)fileName :(const char *)fileType :(const char *)data {
+- (bool)CreateFileAtPath:(const char *)directoryPath :(const char *)fileName :(const char *)contents {
     [self CreateDirectoryAtPath:directoryPath];
 
-    NSString *filePath = [[NSString stringWithUTF8String:(directoryPath)] stringByAppendingPathComponent:
-            directoryPath != nil
-            ? [NSString stringWithFormat:@"/%@.%@", [NSString stringWithUTF8String:(fileName)], [NSString stringWithUTF8String:(fileType)]]
-            : [NSString stringWithFormat:@"/%@", [NSString stringWithUTF8String:(fileName)]]];
+    NSString *filePath = [[self CStringToNSString:directoryPath] stringByAppendingPathComponent:
+            [NSString stringWithFormat:@"/%@", [self CStringToNSString:fileName]]];
 
+    return [self CreateFileAtPath:[self NSStringToCString:filePath] :contents];
+}
 
-    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
-        CBLLog(@"[NSFileManager]: file already exists!");
-    } else {
-        // Create and write to file
-        if ([[NSFileManager defaultManager] createFileAtPath:filePath contents:[NSData dataWithBytes:(const void *) data length:(NSUInteger) strlen(
-                data)]                                                attributes:nil]) {
-            CBLLog(@"[NSFileManager]: file created successfully!");
+- (bool)CreateFileAtPath:(const char *)filePath :(const char *)contents {
+    bool success = false;
+    char *directoryPath = (char *) std::malloc((std::strlen(filePath) * sizeof(filePath)));
+    std::strcpy(directoryPath, (char *) [self GetDirectoryPath:filePath]);
+
+    if ([self CreateDirectoryAtPath:directoryPath]) {
+        if ([self FileExistsAtPath:filePath]) {
+            CBLLog(@"[CreateFileAtPath]: file already exists!");
         } else {
-            CBLLog(@"[NSFileManager]: file creation failed!");
+            NSString *textToWrite = [[NSString alloc] initWithCString:[self WrapTimeStamp:contents]
+                                                             encoding:NSUTF8StringEncoding];
+            NSData *dataToWrite = [NSData dataWithBytes:[self NSStringToCString:textToWrite]
+                                                 length:textToWrite.length];
+            CBLLog(@"[CreateFileAtPath]: textToWrite.length == %lu", (unsigned long) textToWrite.length);
+
+            // Create file
+            if ([mFileManager createFileAtPath:[self CStringToNSString:filePath]
+                                      contents:dataToWrite attributes:nil]) {
+                success = true;
+                mLengthOfLastWrite = dataToWrite ? dataToWrite.length : 0;
+                CBLLog(@"[CreateFileAtPath]: file created successfully!");
+            } else {
+                CBLLog(@"[CreateFileAtPath]: file creation failed!");
+            }
+        }
+
+        if (![self OpenFileAtPath:(filePath)]) {
+            CBLLog(@"[CreateFileAtPath]: Could not open file!");
         }
     }
 
-    mFilePath = filePath.fileSystemRepresentation;
+    return success;
+}
 
-    return mFilePath;
+- (bool)CreateFileAtPath:(const char *)filePath {
+    return [self CreateFileAtPath:filePath :NULL];
+}
+
+- (bool)CreateFileAtDefaultPath {
+    return [self CreateFileAtPath:[self NSStringToCString:mFilePath]];
 }
 
 // Overwrites content of file.
-- (BOOL)OverwriteFileAtPath:(const char *)filePath :(const char *)data {
-    NSData *newData = [NSData dataWithBytes:(const void *) data length:(NSUInteger) strlen(data)];
+- (bool)OverwriteFileAtPathWithContent:(const char *)filePath :(const char *)contents {
+    NSData *dataToWrite = [NSData dataWithBytes:contents length:(NSUInteger) strlen(contents)];
     NSError *error = 0;
 
-    if (![[NSFileManager defaultManager] isWritableFileAtPath:[NSString stringWithUTF8String:(filePath)]]) {
-        CBLLog(@"[Error]: Cannot write to file!");
+    if (![mFileManager isWritableFileAtPath:[self CStringToNSString:filePath]]) {
+        CBLLog(@"[OverwriteFileAtPathWithContent]: Cannot write to file!");
     } else {
-        NSFileHandle *fileHandle = [NSFileHandle fileHandleForUpdatingAtPath:[NSString stringWithUTF8String:(filePath)]];
-        [fileHandle seekToFileOffset:0];
+        if ([self OpenFileAtPath:filePath]) {
+            if ([mFileHandle writeData:dataToWrite error:&error]) {
+                mLengthOfLastWrite = dataToWrite ? std::strlen(contents) : 0;
+                mFileContentsLength += mLengthOfLastWrite;
+                CBLLog(@"[OverwriteFileAtPathWithContent]: Write successful!");
+            } else {
+                CBLLog(@"[OverwriteFileAtPathWithContent]: Error writing to file: %@", error);
+            }
 
-        if ([fileHandle writeData:newData error:&error]) {
-            CBLLog(@"[NSFileHandle]: Write successful!");
+            // This will shrink the file if data was removed
+            [mFileHandle truncateFileAtOffset:dataToWrite.length];
+            [mFileHandle closeFile];
         } else {
-            CBLLog(@"[NSFileHandle]: Error writing to file: %@", error);
+            CBLLog(@"[OverwriteFileAtPathWithContent]: Could not open file!");
         }
-
-        // This will shrink the file if data was removed
-        [fileHandle truncateFileAtOffset:newData.length];
-        [fileHandle closeFile];
     }
 
     return !error ? true : false;
 }
 
 // Appends to contents of file.
-- (BOOL)AppendFileAtPath:(const char *)filePath :(const char *)data {
+- (bool)AppendFileAtPathWithContent:(const char *)filePath :(const char *)contents {
     NSError *error = 0;
 
-    if (![[NSFileManager defaultManager] isWritableFileAtPath:[NSString stringWithUTF8String:(filePath)]]) {
-        CBLLog(@"[Error]: Cannot write to file!");
-        return false;
+    if (filePath == nil) {
+        CBLLog(@"[AppendFileAtPathWithContent]: Invalid filepath!");
+        error = [NSError alloc];
     } else {
-        NSFileHandle *fileHandle = [NSFileHandle fileHandleForUpdatingAtPath:[NSString stringWithUTF8String:(filePath)]];
-        auto offset = fileHandle.seekToEndOfFile;
-        [fileHandle seekToFileOffset:offset];
-
-        NSData *dataToWrite = [NSData dataWithBytes:(const void *) data length:(NSUInteger) strlen(data)];
-
-        if ([fileHandle writeData:(NSData *) dataToWrite error:&error]) {
-            CBLLog(@"[NSFileHandle]: Write successful!");
+        if (![self FileExistsAtPath:filePath]) { // File was deleted during program execution.
+            [self CreateFileAtPath:filePath :contents];
+        } else if (![mFileManager isWritableFileAtPath:[self CStringToNSString:filePath]]) {
+            CBLLog(@"[AppendFileAtPathWithContent]: Cannot write to file!");
+            error = [NSError alloc];
         } else {
-            CBLLog(@"[NSFileHandle]: Error writing to file: %@", error);
-        }
+            NSString *textToWrite = [[NSString alloc] initWithCString:[self WrapTimeStamp:contents]
+                                                             encoding:NSUTF8StringEncoding];
+            NSData *dataToWrite = [NSData dataWithBytes:[self NSStringToCString:textToWrite]
+                                                 length:textToWrite.length];
+            CBLLog(@"[CreateFileAtPath]: textToWrite.length == %lu", (unsigned long) textToWrite.length);
 
-        [fileHandle closeFile];
+            if ([self OpenFileAtPath:filePath]) {
+                [mFileHandle seekToEndOfFile];
+
+                if ([mFileHandle writeData:dataToWrite error:&error]) {
+                    mLengthOfLastWrite = dataToWrite ? dataToWrite.length : 0;
+                    mFileContentsLength += mLengthOfLastWrite;
+                    CBLLog(@"[AppendFileAtPathWithContent]: Write successful!");
+                } else {
+                    CBLLog(@"[AppendFileAtPathWithContent]: Error writing to file: %@", error);
+                }
+
+                [mFileHandle closeFile];
+            } else {
+                CBLLog(@"[AppendFileAtPathWithContent]: Could not open file!");
+            }
+        }
     }
 
     return !error ? true : false;
 }
 
-- (BOOL)RemoveTextFromFileAtPath:(const char *)filePath :(const char *)data {
-    // [fileHandle truncateFileAtOffset:data.length]; // This will shrink the file if data was removed
+- (bool)RemoveTextFromFileAtPath:(const char *)filePath :(const char *)contents {
+    // [mFileHandle truncateFileAtOffset:data.length]; // This will shrink the file if data was removed
+    return false;
+}
 
+// Shows file contents as contents are updated
+- (void)TailFile:(const char *)filePath {
+    if ([self OpenFileAtPath:filePath]) {
+        [mFileHandle seekToFileOffset:(mFileContentsLength - mLengthOfLastWrite)];
+        NSData *data = [mFileHandle readDataToEndOfFile];
+        NSString *contents = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        std::cout << [self RemoveLeadingWhiteSpace:[self NSStringToCString:contents]] << std::endl
+                  << std::flush;
+    }
+}
+
+- (const char *)ReadFileStream:
+        (const char *)filePath {
+    if ([self OpenFileAtPath:filePath]) {
+        NSMutableData *data = [NSMutableData dataWithContentsOfFile:mFilePath];
+        NSString *contents = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        std::cout << [self RemoveLeadingWhiteSpace:[self NSStringToCString:contents]] << std::endl
+                  << std::flush;
+    }
+}
+
+
+- (const char *)RemoveLeadingWhiteSpace:(const char *)contents {
+    char *output;
+    UINT i = 0;
+    UINT j = 0;
+
+    while ((contents[i] == ' ' || contents[i] == '\n') && (contents[i] != '\0')) {
+        ++i;
+    }
+
+    output = (char *) std::malloc((std::strlen(contents - i) * sizeof(contents - i)) + 1);
+
+    while (contents[i] != '\0') {
+        output[j++] = contents[i++];
+    }
+
+    output[j] = '\0';
+
+    return output;
+}
+
+- (const char *)WrapTimeStamp:(const char *)contents {
+    NSString *toReturn = @"[";
+    char *time_stamp = (char *) mTime.GetCurrentTime();
+
+    time_stamp[std::strlen(time_stamp) - 1] = '\0';
+
+    toReturn = [toReturn stringByAppendingFormat:@"%s]:\n", time_stamp];
+    if (contents) {
+        toReturn = [toReturn stringByAppendingFormat:@"%s", contents];
+    }
+    toReturn = [toReturn stringByAppendingString:[self CStringToNSString:"\n\n"]];
+
+    return [self NSStringToCString:toReturn];
 }
 
 @end
+
+
