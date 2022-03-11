102c0f8615257ad534ccb6284c29328348c056ec
broel <joelthomas.e@gmail.com>
2022-03-11 09:07:26 -0800

    Optimized ShowTable(); PopulateTable() now correctly sets clipboard with the contents of the selected row; Added PopulateRandomEntries() to populate EntryList with strings for testing purposes <-- called by PopulateTable()

--- src/Core/CBLEntryList.h
+++ src/Core/CBLEntryList.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "CBLEntryNode.h"
+#include "CBLClipboard.h"
+
+#include "imgui.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,24 +11,36 @@ extern "C" {
 
 @interface CBLEntryList : NSObject {
     std::vector<CBLEntryNode *> mEntryNodes;
+    CBLClipboard* mClipboard;
+
+    ImVector<CBLEntryNode*> mSelection;
 }
 enum ContentsType {
     CT_Text, CT_Button, CT_SmallButton, CT_FillButton, CT_Selectable, CT_SelectableSpanRow
 };
 - (id)init;
 
-- (bool)AddNodeToEntries:(CBLEntryNode *)entryNode;
+- (CBLEntryList *)GetEntryList;
 
-- (int)GetSize;
-+ (CBLEntryList *)GetEntryList;
-- (std::vector<CBLEntryNode *>)GetNodes;
+- (bool)AddNodeToEntries:(CBLEntryNode *)entryNode;
 - (CBLEntryNode *)GetEntryAtIndex:(int)index;
+- (std::vector<CBLEntryNode *>)GetNodes;
+- (int)GetSize;
 
 - (void)ShowTable;
 - (void)PopulateTable;
 - (void)SortEntries;
+- (void)UpdateList;
+
+// TEST ONLY
+- (void)PopulateRandomEntries;
 
 @end
+
+// Custom ImGui functions
+namespace ImGui {
+    void SelectableWrapped();
+}
 
 #ifdef __cplusplus
 }
--- src/Core/CBLEntryList.mm
+++ src/Core/CBLEntryList.mm
@@ -1,12 +1,11 @@
 #import "CBLEntryList.h"
 
-#include "imgui.h"
-
 @implementation CBLEntryList
 
 - (id)init {
     @autoreleasepool {
         self = [super init];
+        mClipboard = [[CBLClipboard alloc] init];
         return self;
     }
 }
@@ -17,9 +16,18 @@
     return true;
 }
 
-+ (CBLEntryList *)GetEntryList {
+- (CBLEntryList *)GetEntryList {
     if (!self) { return [[CBLEntryList alloc] init]; }
     return (CBLEntryList *) self;
+}
+
+- (void)UpdateList {
+    if ([mClipboard UpdateClipboardText]) {
+        NSString *timestamp = [[mClipboard GetTimeStamp] mutableCopy];
+        NSString *clipboard_text = [[mClipboard GetClipboardText] mutableCopy];
+        CBLEntryNode *node = [CBLEntryNode CreateEntryNode:timestamp :clipboard_text];
+        [self AddNodeToEntries:node];
+    }
 }
 
 - (void)SortEntries {
@@ -37,122 +45,108 @@
     return mEntryNodes;
 }
 
+- (void)PopulateRandomEntries {
+    std::vector<std::string>
+            stringBank = {
+            {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+            {"porttitor eget dolor morbi non"},
+            {"neque ornare aenean euismod elementum"},
+            {"ac auctor augue mauris augue neque gravida in"},
+            {"Lobortis mattis aliquam faucibus purus in massa tempor nec feugiat."},
+            {"Faucibus in ornare quam viverra orci sagittis eu volutpat."}};
+
+    for (std::string str: stringBank) {
+        CBLEntryNode *node = [CBLEntryNode CreateEntryNode:[mClipboard GetTimeStamp]
+                :[NSString stringWithUTF8String:str.c_str()]];
+        mEntryNodes.push_back(node);
+    }
+}
+
+
 - (void)ShowTable {
     // Using those as a base value to create width/height that are factor of the size of our font
     const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
     const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
 
-    // Update item list if we changed the number of mEntryNodes
-    bool items_need_sort = false;
     int freeze_cols = 1;
     int freeze_rows = 1;
-    static ImVec2 outer_size_value = ImVec2(0.0f, TEXT_BASE_HEIGHT * 12);
-    static float inner_width_with_scroll = 0.0f; // Auto-extend
-    static bool outer_size_enabled = false;
-    static bool show_headers = true;
-    static bool show_wrapped_text = true;
 
     // Options
-    static ImGuiTableFlags flags =
-            ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable |
-            ImGuiTableFlags_Sortable | ImGuiTableFlags_SortMulti
-            | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV |
-            ImGuiTableFlags_NoBordersInBody
-            | ImGuiTableFlags_ScrollY;
+    static ImGuiTableFlags flags = ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_RowBg |
+                                   ImGuiTableFlags_NoBordersInBody | ImGuiTableFlags_ScrollY;
 
     // Submit table
-    const float inner_width_to_use = (flags & ImGuiTableFlags_ScrollX) ? inner_width_with_scroll : 0.0f;
-    if (ImGui::BeginTable("Entry List", 2, flags, outer_size_enabled ? outer_size_value : ImVec2(0, 0),
-                          inner_width_to_use)) {
+    if (ImGui::BeginTable("Entry Table", 2, flags, ImVec2(0, 0), 0.0f)) {
         // Declare columns
         // We use the "user_id" parameter of TableSetupColumn() to specify a user id that will be stored in the sort specifications.
-        ImGui::TableSetupColumn("Date", ImGuiTableColumnFlags_NoSort | ImGuiTableColumnFlags_WidthFixed |
-                                        ImGuiTableColumnFlags_NoHide, 0.0f, EntryNodeColumnID_Date);
+        ImGui::TableSetupColumn("Timestamp", ImGuiTableColumnFlags_NoSort | ImGuiTableColumnFlags_WidthFixed |
+                                             ImGuiTableColumnFlags_NoHide, 0.0f, EntryNodeColumnID_Timestamp);
         static ImGuiTableFlags contents_flags = ImGuiTableColumnFlags_NoSort | ImGuiTableColumnFlags_WidthStretch;
         ImGui::TableSetupColumn("Contents", contents_flags, 0.0f, EntryNodeColumnID_Contents);
         ImGui::TableSetupScrollFreeze(freeze_cols, freeze_rows);
 
-        // Sort our data if sort specs have been changed!
-        // ImGuiTableSortSpecs *sorts_specs = ImGui::TableGetSortSpecs();
-        // if (sorts_specs && sorts_specs->SpecsDirty) {
-        //     items_need_sort = true;
-        // }
-
-        // if (sorts_specs && items_need_sort) {
-        //     CBLEntryNode::s_current_sort_specs = sorts_specs; // Store in variable accessible by the sort function.
-        //     qsort(mEntryNodes.data(), mEntryNodes.size(), sizeof(mEntryNodes), CBLEntryNode::CompareWithSortSpecs);
-        //     CBLEntryNode::s_current_sort_specs = NULL;
-        //     sorts_specs->SpecsDirty = false;
-        // }
-        // items_need_sort = false;
-
-        // Take note of whether we are currently sorting based on the Quantity field,
-        // we will use this to trigger sorting when we know the data of this column has been modified.
-        const bool sorts_specs_using_date = (ImGui::TableGetColumnFlags(0) & ImGuiTableColumnFlags_IsSorted) != 0;
-
         // Show headers
-        if (show_headers) {
-            ImGui::TableHeadersRow();
-        }
-
-        ImGui::PushButtonRepeat(true);
+        ImGui::TableHeadersRow();
         {
             [self PopulateTable];
         }
-        ImGui::PopButtonRepeat();
         ImGui::EndTable();
     }
 }
 
 - (void)PopulateTable {
-    ImVector<const char *> selection;
-    float row_min_height = 0.0f; // Auto
-    static int contents_type = CT_SelectableSpanRow;
 
+    // ======= Testing purposes only ======== //
+    static bool called = false;
+    if (!called) {
+        [self PopulateRandomEntries];
+        called = true;
+    }
+    // ====================================== //
+
+    // Begin function
+    [self UpdateList];
+    float row_min_height = 0.0f; // Auto
+
+    // Populate the table with data
     for (int row_n = 0; row_n < mEntryNodes.size(); row_n++) {
-        // Populate the table with data
         CBLEntryNode *item = mEntryNodes[row_n];
         const char *timestamp = [[item GetTimestamp] UTF8String];
-        const bool item_is_selected = selection.contains(timestamp);
         ImGui::PushID(timestamp);
         ImGui::TableNextRow(ImGuiTableRowFlags_None, row_min_height);
 
         ImGui::TableSetColumnIndex(0);
-        char label[32];
-        sprintf(label, "%s", timestamp);
-        if (contents_type == CT_Text) {
-            ImGui::TextUnformatted(label);
-        } else if (contents_type == CT_Button) {
-            ImGui::Button(label);
-        } else if (contents_type == CT_SmallButton) {
-            ImGui::SmallButton(label);
-        } else if (contents_type == CT_FillButton) {
-            ImGui::Button(label, ImVec2(-FLT_MIN, 0.0f));
-        } else if (contents_type == CT_Selectable || contents_type == CT_SelectableSpanRow) {
-            ImGuiSelectableFlags selectable_flags = (contents_type == CT_SelectableSpanRow) ?
-                                                    ImGuiSelectableFlags_SpanAllColumns |
-                                                    ImGuiSelectableFlags_AllowItemOverlap
-                                                                                            : ImGuiSelectableFlags_None;
-            if (ImGui::Selectable(label, item_is_selected, selectable_flags, ImVec2(0, row_min_height))) {
-                if (ImGui::GetIO().KeyCtrl) {
-                    if (item_is_selected) {
-                        selection.find_erase_unsorted(timestamp);
-                    } else {
-                        selection.push_back(timestamp);
-                    }
-                } else {
-                    selection.clear();
-                    selection.push_back(timestamp);
-                }
-            }
-        }
+        ImGui::TextWrapped("%s", timestamp);
+
+        const char *contents = [[item GetContents] UTF8String];
+        const bool item_is_selected = mSelection.contains(item);
 
         if (ImGui::TableSetColumnIndex(1)) {
-            ImGui::TextWrapped("%s", [[item GetContents] UTF8String]);
-        }
 
+            // TODO: Wrap text w/in selectable
+            if (ImGui::Selectable(contents, item_is_selected, ImGuiSelectableFlags_SpanAllColumns,
+                                  ImVec2(0, row_min_height))) {
+                [mClipboard SetClipboardText:contents];
+                mSelection.clear();
+                mSelection.push_back(item);
+            }
+        }
         ImGui::PopID();
     }
 }
 
 @end
+
+namespace ImGui {
+
+    // This function will eventually allow for wrapped text w/in a selectable
+    bool SelectableWrapped(const char* label, bool selected = false, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0)) {
+        // const float text_size = ImGui::CalcTextSize("A").x;
+        //
+        // Selectable("##hidden", selected, flags, ImVec2(text_size, size.y));
+
+        // You might calc CalcTextSizeA() with wrapping setting to obtain a size,
+        // use this size to create a Selectable() with an empty label (e.g. Selectable("##hidden", size))
+        // then using ImDrawList->AddText() to render the text over it.
+    }
+}
