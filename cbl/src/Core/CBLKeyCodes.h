305e95b5422e71695919c05e56a4f57fdb1ed573
broel <joelthomas.e@gmail.com>
2022-03-18 13:53:14 -0700

    Abstracted keycodes for key events

---
+++ cbl/src/Core/CBLKeyCodes.h
@@ -0,0 +1,125 @@
+#pragma once
+
+// From glfw3.h
+#define CBL_KEY_SPACE              32
+#define CBL_KEY_APOSTROPHE         39  /* ' */
+#define CBL_KEY_COMMA              44  /* , */
+#define CBL_KEY_MINUS              45  /* - */
+#define CBL_KEY_PERIOD             46  /* . */
+#define CBL_KEY_SLASH              47  /* / */
+#define CBL_KEY_0                  48
+#define CBL_KEY_1                  49
+#define CBL_KEY_2                  50
+#define CBL_KEY_3                  51
+#define CBL_KEY_4                  52
+#define CBL_KEY_5                  53
+#define CBL_KEY_6                  54
+#define CBL_KEY_7                  55
+#define CBL_KEY_8                  56
+#define CBL_KEY_9                  57
+#define CBL_KEY_SEMICOLON          59  /* ; */
+#define CBL_KEY_EQUAL              61  /* = */
+#define CBL_KEY_A                  65
+#define CBL_KEY_B                  66
+#define CBL_KEY_C                  67
+#define CBL_KEY_D                  68
+#define CBL_KEY_E                  69
+#define CBL_KEY_F                  70
+#define CBL_KEY_G                  71
+#define CBL_KEY_H                  72
+#define CBL_KEY_I                  73
+#define CBL_KEY_J                  74
+#define CBL_KEY_K                  75
+#define CBL_KEY_L                  76
+#define CBL_KEY_M                  77
+#define CBL_KEY_N                  78
+#define CBL_KEY_O                  79
+#define CBL_KEY_P                  80
+#define CBL_KEY_Q                  81
+#define CBL_KEY_R                  82
+#define CBL_KEY_S                  83
+#define CBL_KEY_T                  84
+#define CBL_KEY_U                  85
+#define CBL_KEY_V                  86
+#define CBL_KEY_W                  87
+#define CBL_KEY_X                  88
+#define CBL_KEY_Y                  89
+#define CBL_KEY_Z                  90
+#define CBL_KEY_LEFT_BRACKET       91  /* [ */
+#define CBL_KEY_BACKSLASH          92  /* \ */
+#define CBL_KEY_RIGHT_BRACKET      93  /* ] */
+#define CBL_KEY_GRAVE_ACCENT       96  /* ` */
+#define CBL_KEY_WORLD_1            161 /* non-US #1 */
+#define CBL_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define CBL_KEY_ESCAPE             256
+#define CBL_KEY_ENTER              257
+#define CBL_KEY_TAB                258
+#define CBL_KEY_BACKSPACE          259
+#define CBL_KEY_INSERT             260
+#define CBL_KEY_DELETE             261
+#define CBL_KEY_RIGHT              262
+#define CBL_KEY_LEFT               263
+#define CBL_KEY_DOWN               264
+#define CBL_KEY_UP                 265
+#define CBL_KEY_PAGE_UP            266
+#define CBL_KEY_PAGE_DOWN          267
+#define CBL_KEY_HOME               268
+#define CBL_KEY_END                269
+#define CBL_KEY_CAPS_LOCK          280
+#define CBL_KEY_SCROLL_LOCK        281
+#define CBL_KEY_NUM_LOCK           282
+#define CBL_KEY_PRINT_SCREEN       283
+#define CBL_KEY_PAUSE              284
+#define CBL_KEY_F1                 290
+#define CBL_KEY_F2                 291
+#define CBL_KEY_F3                 292
+#define CBL_KEY_F4                 293
+#define CBL_KEY_F5                 294
+#define CBL_KEY_F6                 295
+#define CBL_KEY_F7                 296
+#define CBL_KEY_F8                 297
+#define CBL_KEY_F9                 298
+#define CBL_KEY_F10                299
+#define CBL_KEY_F11                300
+#define CBL_KEY_F12                301
+#define CBL_KEY_F13                302
+#define CBL_KEY_F14                303
+#define CBL_KEY_F15                304
+#define CBL_KEY_F16                305
+#define CBL_KEY_F17                306
+#define CBL_KEY_F18                307
+#define CBL_KEY_F19                308
+#define CBL_KEY_F20                309
+#define CBL_KEY_F21                310
+#define CBL_KEY_F22                311
+#define CBL_KEY_F23                312
+#define CBL_KEY_F24                313
+#define CBL_KEY_F25                314
+#define CBL_KEY_KP_0               320
+#define CBL_KEY_KP_1               321
+#define CBL_KEY_KP_2               322
+#define CBL_KEY_KP_3               323
+#define CBL_KEY_KP_4               324
+#define CBL_KEY_KP_5               325
+#define CBL_KEY_KP_6               326
+#define CBL_KEY_KP_7               327
+#define CBL_KEY_KP_8               328
+#define CBL_KEY_KP_9               329
+#define CBL_KEY_KP_DECIMAL         330
+#define CBL_KEY_KP_DIVIDE          331
+#define CBL_KEY_KP_MULTIPLY        332
+#define CBL_KEY_KP_SUBTRACT        333
+#define CBL_KEY_KP_ADD             334
+#define CBL_KEY_KP_ENTER           335
+#define CBL_KEY_KP_EQUAL           336
+#define CBL_KEY_LEFT_SHIFT         340
+#define CBL_KEY_LEFT_CONTROL       341
+#define CBL_KEY_RIGHT_ALT          342
+#define CBL_KEY_RIGHT_SUPER        343
+#define CBL_KEY_RIGHT_SHIFT        344
+#define CBL_KEY_RIGHT_CONTROL      345
+#define CBL_KEY_LEFT_ALT           346
+#define CBL_KEY_LEFT_SUPER         347
+#define CBL_KEY_MENU               348
